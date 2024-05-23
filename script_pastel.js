94b0408574f80e885f0481d527f43d7531e3ae6c
darkrowaccess <145072372+darkrowaccess@users.noreply.github.com>
2024-05-23 19:48:34 -0300

    Update script_pastel.js

--- script_pastel.js
+++ script_pastel.js
@@ -1,1 +1,12 @@
+function shine(element) {
+    var shineEffect = document.createElement("div");
+    shineEffect.classList.add("shine");
+    element.appendChild(shineEffect);
 
+    setTimeout(function() {
+        shineEffect.classList.add("active");
+        setTimeout(function() {
+            shineEffect.remove();
+        }, 1000); // Modifica el tiempo de duración del efecto si lo deseas
+    }, 50);
+}
